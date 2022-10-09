67bc9364157b5bfcda6ccf61cad6deba635e9d11
DEEP KAMLESHKUMAR DAVE <dp724555@dal.ca>
2022-10-09 16:04:48 -0300

    Test ci-cd pipeline

---
+++ src/main/java/Group3/JobsMadeEasy/JobsMadeEasyApplication.java
@@ -0,0 +1,15 @@
+package Group3.JobsMadeEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsMadeEasyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JobsMadeEasyApplication.class, args);
+
+	}
+
+}
