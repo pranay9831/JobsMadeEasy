a1eea1a3b782ce045cdc5fb0a8d29bccee46b8be
DEEP KAMLESHKUMAR DAVE <dp724555@dal.ca>
2022-11-25 19:25:22 -0400

    remove unused import

--- src/main/java/Group3/JobsMadeEasy/authentication/applicant/repository/login/ApplicantLoginRepositoryImp.java
+++ src/main/java/Group3/JobsMadeEasy/authentication/applicant/repository/login/ApplicantLoginRepositoryImp.java
@@ -4,14 +4,11 @@ import Group3.JobsMadeEasy.authentication.applicant.model.Applicant;
 import Group3.JobsMadeEasy.authentication.applicant.model.Login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Repository
 public class ApplicantLoginRepositoryImp extends JdbcDaoSupport implements IApplicantLoginRepository{
