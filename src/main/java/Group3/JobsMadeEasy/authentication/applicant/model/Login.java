415948240b10e720014fc01eb1528447656c626d
DEEP KAMLESHKUMAR DAVE <dp724555@dal.ca>
2022-11-25 19:19:57 -0400

    added applicant register and login

---
+++ src/main/java/Group3/JobsMadeEasy/authentication/applicant/model/Login.java
@@ -0,0 +1,31 @@
+package Group3.JobsMadeEasy.authentication.applicant.model;
+
+public class Login {
+
+    private String emailId;
+    private String password;
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "emailId='" + emailId + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/Group3/JobsMadeEasy/authentication/applicant/repository/login/ApplicantLoginRepositoryImp.java
@@ -0,0 +1,53 @@
+package Group3.JobsMadeEasy.authentication.applicant.repository.login;
+
+import Group3.JobsMadeEasy.authentication.applicant.model.Applicant;
+import Group3.JobsMadeEasy.authentication.applicant.model.Login;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class ApplicantLoginRepositoryImp extends JdbcDaoSupport implements IApplicantLoginRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+    DataSource dataSource;
+
+    @Autowired
+    public ApplicantLoginRepositoryImp(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.dataSource = dataSource;
+    }
+
+    @PostConstruct
+    private void initialize() {
+        setDataSource(dataSource);
+    }
+
+    @Override
+    public Applicant checkLoginDetails(Login login) {
+        System.out.println(login.getEmailId() + ">>>>" + login.getPassword());
+        String sql = "SELECT * FROM applicant WHERE emailId = ? AND  password = ?";
+        return getJdbcTemplate().queryForObject(sql, (rs, rwNumber) -> {
+            Applicant applicant = new Applicant();
+            applicant.setApplicantId(rs.getInt("applicantId"));
+            applicant.setFirstName(rs.getString("firstName"));
+            applicant.setLastName(rs.getString("lastName"));
+            applicant.setPhoneNumber(rs.getString("phoneNumber"));
+            applicant.setEmailId(rs.getString("emailId"));
+            applicant.setPassword(rs.getString("password"));
+            applicant.setCity(rs.getString("city"));
+            applicant.setProvince(rs.getString("province"));
+            applicant.setAddress(rs.getString("address"));
+            applicant.setPostalCode(rs.getString("postalCode"));
+            applicant.setApplicantStatus(rs.getBoolean("applicantStatus"));
+            return applicant;
+        },new Object[]{login.getEmailId(),login.getPassword()});
+    }
+}
