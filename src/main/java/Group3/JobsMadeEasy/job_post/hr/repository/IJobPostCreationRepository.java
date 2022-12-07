963ec07076eef454ba237c155e233f6c54238d63
Neha Ninad Karkhanis <nh601176@dal.ca>
2022-12-07 15:59:52 -0400

    integrated themeleaf for authentication

--- src/main/java/Group3/JobsMadeEasy/job_post/hr/repository/IJobPostCreationRepository.java
+++
@@ -1,9 +0,0 @@
-package Group3.JobsMadeEasy.job_post.hr.repository;
-
-
-import Group3.JobsMadeEasy.job_post.hr.model.JobPost;
-
-public interface IJobPostCreationRepository
-{
-    boolean createJobPost (JobPost jobPost);
-}
